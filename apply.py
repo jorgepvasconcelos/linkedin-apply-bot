2f8cbbfe94a53182ef06f721642e9219bbdb646a
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-08-18 22:00:47 -0300

    feat: login

---
+++ apply.py
@@ -0,0 +1,60 @@
+import time
+
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+from selenium_toolkit import SeleniumToolKit
+
+from settings import USERNAME, PASSWORD, KEYWORD, LOCATION
+
+
+class LinkedinApplyBot:
+    def __init__(self, selenium_kit: SeleniumToolKit):
+        self.sk = selenium_kit
+
+    def run(self):
+        self.login()
+        self.search_jobs()
+
+    def login(self):
+        login_url = 'https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin'
+        self.sk.goto(url=login_url)
+
+        self.sk.fill(text=USERNAME, locator=(By.CSS_SELECTOR, '[id="username"]'))
+        self.sk.fill(text=PASSWORD, locator=(By.CSS_SELECTOR, '[id="password"]'))
+        self.sk.click(locator=(By.CSS_SELECTOR, '[type="submit"]'))
+
+        return self.sk.element_is_present(wait_time=5, locator=(By.CSS_SELECTOR, '.identity-headline'))
+
+    def search_jobs(self):
+        search_url = 'https://www.linkedin.com/jobs/search'
+        self.sk.goto(url=search_url)
+
+        self.sk.fill(text=KEYWORD, locator=(By.CSS_SELECTOR, '[id="jobs-search-box-keyword-id-ember24"]'))
+        self.sk.fill(text=LOCATION, locator=(By.CSS_SELECTOR, '[id="jobs-search-box-location-id-ember24"]'))
+        self.sk.click(locator=(By.CSS_SELECTOR, '.jobs-search-box__submit-button'))
+
+        return self.sk.element_is_present(wait_time=5, locator=(By.CSS_SELECTOR, '.identity-headline'))
+
+
+    def search_jobs2(self):
+        search_url = 'https://www.linkedin.com/jobs/search/?keywords=python&location=Portugal&refresh=true'
+        self.sk.goto(url=search_url)
+
+        self.sk.fill(text=KEYWORD, locator=(By.CSS_SELECTOR, '[id="jobs-search-box-keyword-id-ember24"]'))
+        self.sk.fill(text=LOCATION, locator=(By.CSS_SELECTOR, '[id="jobs-search-box-location-id-ember24"]'))
+        self.sk.click(locator=(By.CSS_SELECTOR, '.jobs-search-box__submit-button'))
+
+        return self.sk.element_is_present(wait_time=5, locator=(By.CSS_SELECTOR, '.identity-headline'))
+
+
+if __name__ == '__main__':
+    options = Options()
+    options.add_argument('--start-maximized')
+
+    driver = Chrome(options=options)
+    selenium_kit = SeleniumToolKit(driver=driver)
+
+    bot = LinkedinApplyBot(selenium_kit=selenium_kit)
+    bot.run()
---
+++ settings.py
@@ -0,0 +1,5 @@
+USERNAME = 'kuririn@hotmail.com'
+PASSWORD = '1238545'
+
+KEYWORD = 'python'
+LOCATION = 'Brasil'
